41f55ea57da13926da6afbaec9bc6e02e7ad6f82
IvanDiazSilva <ivandiazsilva70@gmail.com>
2026-08-04 12:09:57 +0200

    chore(productos): eliminar import computed sin usar en productos-list

--- src/app/features/productos/productos-list/productos-list.component.ts
+++ src/app/features/productos/productos-list/productos-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal, computed } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
